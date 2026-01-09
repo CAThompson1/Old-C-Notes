d3dbb669d932a21d98b0ce91bdccf71811b95993
Prysmil <prysmilprofessional@gmail.com>
2026-01-09 13:45:57 -0500

    Add files via upload
    Round 5

---
+++ myattempt at making notes for day 7/VehicleExample.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class VehicleExample
+{
+
+	private string mMake;
+	private string mModel;
+	private int mYear;
+
+	private int mMileage
+	
+	
+	//C# Properties
+	public string Make
+	{
+		get
+		{
+			return mMake;
+		}
+	}	
+		
+		
+		
+		
+		//defult constructor
+	public VehicleExample()
+	{
+
+
+	}
+	//Overloaded constructor
+	public VehicleExample(string mMake, string mModel, int mMilage = 0)
+	{
+		mMake = mMake;
+		mModel = model; 
+		mYear = year;
+		mMileage = Milage;
+	}
+	//repeat this for all varibales
+	//ex:GetModel or GetYear
+	public string GetMake()
+	{
+		return mMake;
+	}
+	public string GetDescription()
+	{
+		string condition = (mMileage > 10) ? "Used" : "New";
+		string suffex = (mMileage == 0 || mMileage > 1) ? "s" : "";
+
+		return $"{condition}{mYear} {mMake} {mModel}";
+
+	}
+
+	public void SetMileage(int miles)
+	{
+		if(miles > 0)
+		{
+			mMileage = miles;
+		}
+		
+	}
+
+
+
+
+
+
+	StringBuilder SB = 
+
+}
---
+++ myattempt at making notes for day 7/myattempt at making notes for day 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace myattempt_at_making_notes_for_day_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleExample hondaCivic = new VehicleExample("Honda", "Civic", 2011);
+            VehicleExample fordF150 = new VehicleExample("Ford", "F150", 2013);
+
+
+            //String concatination
+            //Bad example
+            //Console.WriteLine(hondaCivic.mMake +"" + hondaCivic.mModel
+
+
+
+        }
+    }
+}
