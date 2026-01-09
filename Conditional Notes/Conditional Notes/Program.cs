c39568f0e4f624ecc0290d00d5a1cc21455fb2d3
Prysmil <prysmilprofessional@gmail.com>
2026-01-09 13:41:25 -0500

    Add files via upload
    Second upload of notes

---
+++ Conditional Notes/Conditional Notes/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Conditional_Notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (5<3) 
+            {
+            
+                //if the statement above is true then
+                //do this
+            
+            }
+
+            //do this instead
+
+
+             bool LessThanFive(int number)
+            {
+
+                bool result = false;
+                
+                
+                if (number < 5)
+                {
+
+                    result= true;
+                }
+                else
+                {
+                    return false;
+
+                }
+
+
+
+            }
+
+
+            public string GetFileType(string FileName) 
+            {
+                // string splits the filename
+
+                string filext = null;
+
+                if(filext.ToLower() == "txt") 
+                {
+                    filetype = "text file";
+                }
+                else if (filext.ToLower == mp3)
+                {
+                    filetype = "Music File";
+                }
+
+
+
+
+                if(filext != null && number < 100) { }
+            }
+
+
+
+            switch (filetype.ToLower())
+            {
+                case "txt":
+                        filetype = "text file";
+                    break;
+
+                case "mp3":
+                    filetype = "Music File";
+                    break;
+            }
+        }
+    }
+}
+
