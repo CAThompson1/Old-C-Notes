de5f4afcf3351ac0f03981975ea029d13bb17b4c
Prysmil <prysmilprofessional@gmail.com>
2026-01-09 13:47:29 -0500

    Add files via upload
    Round 6

---
+++ NotesForDay8/NotesForDay8/Game.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace NotesForDay8
+{
+    public class Game
+    {
+        private int number;     //number to guess
+        private int maxNumber;      //number ceiling
+        private int guesses;        //amount of tries
+        private int score;
+
+        public Game() 
+        {
+            guesses = 0; 
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Pick a high number!");
+
+            maxNumber = ValidateNumber("High Number");
+
+            int ValidateNumber(string promptText)
+            {
+                int output = 0;
+
+                
+
+                //maxNumber = Console.ReadLine();
+
+                bool validateInput = false;
+                do
+                {
+                    Console.WriteLine("Pick a high number!");
+                    Console.Write("Number: ");
+                    //check for numerical input
+                    validateInput = Int32.TryParse(Console.ReadLine(), out output);
+                    if (!validateInput || output <= 0)
+                    {
+                        Console.WriteLine("We connot accept that input, please try again.");
+                    }
+                    
+                } while (!validateInput || output <= 0);
+
+
+
+                return output;
+            }
+
+            
+           
+
+            Random rnd = new Random();
+
+            number = rnd.Next(1, maxNumber);
+
+            int guess;
+            do
+            {
+                Console.WriteLine("Guess the number!!!!!1!1");
+                guess = ValidateNumber("High Number");
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Too low, try again buddy.");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Too high, try again buddy.");
+                }
+                else
+                {
+                    Console.WriteLine("WOW!");
+                }
+
+                guesses++;
+
+            } while (number != guess);
+
+           
+
+            
+            score = maxNumber + 1 - ((int)Math.Pow(guesses, 2));
+        }
+    }
+}
---
+++ NotesForDay8/NotesForDay8/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NotesForDay8
+{
+   //its actually a practical today :)
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please let me have 3 more hours of sleep");
+           
+            //instance is 
+            Game game = new Game();
+
+            game.Play();
+        }
+
+
+
+    }
+}
