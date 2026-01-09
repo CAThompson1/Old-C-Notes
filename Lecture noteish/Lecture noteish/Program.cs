e32b96cf22761d4997a335602a445fcb495eabe0
Prysmil <prysmilprofessional@gmail.com>
2026-01-09 13:45:07 -0500

    Add files via upload
    Fourth Round of notes

---
+++ Lecture noteish/Lecture noteish/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Lecture_noteish
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             static int Sum(int number1, int number2) 
+         {
+                int sum = number1 + number2; 
+                
+                return sum;
+
+         }
+
+            int sum = Sum(5, 4);
+        }
+    }
+
+
+
+
+    public class Student 
+    {
+        private int age;
+        private double gpa;
+
+        public Student()
+        {
+            age = 18;
+
+            gpa = 3.5;
+        }
+    
+
+        //example of an overloaded construct
+        public Student(int old, double grades) 
+        {
+            age = old;
+            gpa = grades;
+        
+        
+        }
+
+
+    }
+
+
+    public class Student2 
+    {
+        private int age2;
+        private double gpa2;
+
+
+        public int GetAge()
+        {
+
+            return age2;
+        }
+    }
+
+
+    private static void ShowDialog(string text)
+    {
+        Console.Clear();
+        Console.WriteLine(text);
+        Console.ReadKey();
+    }
+
+}
